03f814560c24cc3b53df5f066f7b0e7ccac54ef2
onebarloop <alexanderbayer@posteo.de>
2023-10-24 21:28:11 +0200

    cleaning

--- src/Controller/NotesController.php
+++ src/Controller/NotesController.php
@@ -8,19 +8,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Notes;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
-
-use function PHPUnit\Framework\throwException;
 
 class NotesController extends AbstractController
 {
-
-
-
     #[Route('/notes', name: 'notes')]
     public function defaultAction(EntityManagerInterface $entityManager, Request $request): Response
     {
-
         $error = $request->query->get('failed') == 1 ? true : false;
 
         $repository = $entityManager->getRepository(Notes::class);
@@ -47,7 +40,6 @@ class NotesController extends AbstractController
     public function createNote(EntityManagerInterface $entityManager, Request $request): Response
     {
         $name = $request->query->get('name');
-
         $description = $request->query->get('description');
 
         $note = new Notes();
@@ -72,12 +64,10 @@ class NotesController extends AbstractController
     #[Route('notes/delete/{id}')]
     public function deleteNote(EntityManagerInterface $entityManager, int $id): Response
     {
-
         $note = $entityManager->getRepository(Notes::class)->find($id);
 
         $entityManager->remove($note);
         $entityManager->flush();
-
 
         return $this->redirectToRoute('notes');
     }
